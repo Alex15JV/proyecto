d45388a10d4faca4642b1709114fe894992a1f49
Alex15JV <148788636+Alex15JV@users.noreply.github.com>
2024-04-25 14:46:28 -0400

    Add files via upload

--- Funciones.js
+++ Funciones.js
@@ -70,8 +70,8 @@ function abrirLogin() {
 
 function EliminarProducto(index) {
     carrito.splice(index, 1);
+    actualizarC();
     GuardarP();
-    abrirCarrito();
 }
   
 function cerrarCarrito() {
